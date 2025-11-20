bf534c12dbed7b46091e88d2e038ff821b0cf673
saramenasantamaria <saramenasantamaria@gmail.com>
2025-11-20 13:51:12 +0100

    Update vcfCheck.R

--- R/vcfCheck.R
+++ R/vcfCheck.R
@@ -28,92 +28,99 @@ vcfCheck <- function(
     mother,
     proband,
     check_quality = FALSE) {
-  # Check if `largeCollapsedVcf` is provided
-  
-  if (missing(largeCollapsedVcf)) {
-    stop("Argument 'largeCollapsedVcf' is missing.")
-  }
-  
-  # Check if `largeCollapsedVcf` is a VCF object
-  if (!inherits(largeCollapsedVcf, "CollapsedVCF")) {
-    stop("Argument 'largeCollapsedVcf' must be a VCF object.")
-  }
-  
-  # Check if `proband`,`father` and `mother` is provided
-  
-  if (missing(proband)) {
-    stop("Argument 'proband' is missing.")
-  }
-  if (missing(father)) {
-    stop("Argument 'father' is missing.")
-  }
-  
-  if (missing(mother)) {
-    stop("Argument 'mother' is missing.")
-  }
-  
-  # Check if `proband` ,`father` and `mother` is a character vector
-  if (!inherits(proband, "character")) {
-    stop("Argument 'proband' must be a character vector")
-  }
-  
-  if (!inherits(mother, "character")) {
-    stop("Argument 'mother' must be a character vector")
-  }
-  
-  if (!inherits(father, "character")) {
-    stop("Argument 'father' must be a character vector")
-  }
-  
-  # Extract genotype data
-  geno_data <- VariantAnnotation::geno(largeCollapsedVcf)
-  
-  # Quality parameters
-  if (isTRUE(check_quality)) {
-    if (any(geno_data$GQ < 20 | is.na(geno_data$GQ)) == TRUE) {
-      message("No filter quality (GQ) parameter used")
+    # Check if `largeCollapsedVcf` is provided
+    
+    if (missing(largeCollapsedVcf)) {
+      stop("Argument 'largeCollapsedVcf' is missing.")
     }
-    if (any(geno_data$DP < 30 | is.na(geno_data$DP)) == TRUE) {
-      message("No filter quality (RD) parameter used")
+    
+    # Check if `largeCollapsedVcf` is a VCF object
+    if (!inherits(largeCollapsedVcf, "CollapsedVCF")) {
+      stop("Argument 'largeCollapsedVcf' must be a VCF object.")
     }
-  }
-  
-  # Define allowed genotypes and numeric codes
-  genotypes <- c(
-    "0/0" = "1", "0/1" = "2", "1/0" = "2", "1/1" = "3",
-    "0|0" = "1", "0|1" = "2", "1|0" = "2", "1|1" = "3"
-  )
-  
-  unique_genotypes <- unique(geno_data$GT)
-  
-  if (!all(unique_genotypes %in% names(genotypes))) {
-    invalid_genotypes <- unique_genotypes[!unique_genotypes %in% names(genotypes)]
-    stop(paste("Error: The following genotypes are not valid:", 
-               paste(unique(invalid_genotypes), collapse = ", ")))
-  }
-  
-  # Save original IDs in colData
-  SummarizedExperiment::colData(largeCollapsedVcf)$ID <- colnames(largeCollapsedVcf)
-  
-  # Change names in vcf for subsequent steps
-  colnames(largeCollapsedVcf)[colnames(largeCollapsedVcf) 
-                              == father] <- "father"
-  colnames(largeCollapsedVcf)[colnames(largeCollapsedVcf) 
-                              == mother] <- "mother"
-  colnames(largeCollapsedVcf)[colnames(largeCollapsedVcf) 
-                              == proband] <- "proband"
-  
-  # Generate numeric encoding of trio genotypes
-  geno_uncoded <- VariantAnnotation::geno(largeCollapsedVcf)$GT
-  geno_coded <- paste0(
-    genotypes[geno_uncoded[, "father"]],
-    genotypes[geno_uncoded[, "mother"]],
-    genotypes[geno_uncoded[, "proband"]]
-  )
-  # Each string is 3 characters: father + mother + proband; values: 1=hom_ref, 2=het, 3=hom_alt
-  
-  # Store the numeric genotype string in the VCF metadata column 'geno_coded'
-  S4Vectors::mcols(largeCollapsedVcf)$geno_coded <- geno_coded
-  
-  return(largeCollapsedVcf)
+    
+    # Check if `proband`,`father` and `mother` is provided
+    
+    if (missing(proband)) {
+      stop("Argument 'proband' is missing.")
+    }
+    if (missing(father)) {
+      stop("Argument 'father' is missing.")
+    }
+    
+    if (missing(mother)) {
+      stop("Argument 'mother' is missing.")
+    }
+    
+    
+    # Check if `largeCollapsedVcf` is a VCF object
+    if (!inherits(largeCollapsedVcf, "CollapsedVCF")) {
+      stop("Argument 'largeCollapsedVcf' must be a VCF object.")
+    }
+    
+    # Check if `proband` ,`father` and `mother` is a character vector
+    if (!inherits(proband, "character")) {
+      stop("Argument 'proband' must be a character vector")
+    }
+    
+    if (!inherits(mother, "character")) {
+      stop("Argument 'mother' must be a character vector")
+    }
+    
+    if (!inherits(father, "character")) {
+      stop("Argument 'father' must be a character vector")
+    }
+    # Extract genotype data
+    geno_data <- VariantAnnotation::geno(largeCollapsedVcf)
+    
+    # Quality parameters
+    if (isTRUE(check_quality)) {
+      if (any(geno_data$GQ < 20 | is.na(geno_data$GQ)) == TRUE) {
+        message("No filter quality (GQ) parameter used")
+      }
+      if (any(geno_data$DP < 30 | is.na(geno_data$DP)) == TRUE) {
+        message("No filter quality (RD) parameter used")
+      }
+    }
+    
+    # Define allowed genotypes and numeric codes
+    genotypes <- c(
+      "0/0" = "1", "0/1" = "2", "1/0" = "2", "1/1" = "3",
+      "0|0" = "1", "0|1" = "2", "1|0" = "2", "1|1" = "3"
+    )
+    
+    unique_genotypes <- unique(geno_data$GT)
+    
+    if (!all(unique_genotypes %in% names(genotypes))) {
+      invalid_genotypes <- unique_genotypes[!unique_genotypes %in% names(genotypes)]
+      stop(paste("Error: The following genotypes are not valid:", 
+                 paste(unique(invalid_genotypes), collapse = ", ")))
+    }
+    
+    # Save original IDs in colData
+    SummarizedExperiment::colData(largeCollapsedVcf)$ID <- colnames(largeCollapsedVcf)
+    
+    # Change names in vcf for subsequent steps
+    colnames(largeCollapsedVcf)[colnames(largeCollapsedVcf) 
+                                == father] <- "father"
+    colnames(largeCollapsedVcf)[colnames(largeCollapsedVcf) 
+                                == mother] <- "mother"
+    colnames(largeCollapsedVcf)[colnames(largeCollapsedVcf) 
+                                == proband] <- "proband"
+    
+    # Generate numeric encoding of trio genotypes
+    geno_uncoded <- VariantAnnotation::geno(largeCollapsedVcf)$GT
+    geno_coded <- paste0(
+      genotypes[geno_uncoded[, "father"]],
+      genotypes[geno_uncoded[, "mother"]],
+      genotypes[geno_uncoded[, "proband"]]
+    )
+    # Each string is 3 characters: father + mother + proband; values: 1=hom_ref, 2=het, 3=hom_alt
+    
+    # Store the numeric genotype string in the VCF metadata column 'geno_coded'
+    S4Vectors::mcols(largeCollapsedVcf)$geno_coded <- geno_coded
+
+    
+    return(largeCollapsedVcf)
+    
 }
