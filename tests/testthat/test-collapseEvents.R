a0dd66d5488abbd31e234227e9aa7236a461eb2f
saramenasantamaria <saramenasantamaria@gmail.com>
2026-01-28 23:29:15 +0100

    Update test-collapseEvents.R

--- tests/testthat/test-collapseEvents.R
+++ tests/testthat/test-collapseEvents.R
@@ -1,15 +1,15 @@
 # Input test dataframe
 test_df <- data.frame(
-    ID = c("S1", "S1", "S1", "S1", "S1", "S2", "S2"),
-    chromosome = c("1", "1", "1", "1", "1", "2", "2"),
-    start = c(50, 75, 100, 150, 300, 500, 550),
-    end = c(70, 85, 120, 180, 320, 520, 580),
-    n_snps = c(5, 3, 8, 10, 6, 12, 7),
-    group = c("iso_mat", "iso_mat", "iso_mat", "iso_mat", "het_pat", "iso_mat", "iso_mat"),
-    n_mendelian_error = c(1, 5, 5, 10, 3, 50, 30),
-    ratio_father  = rep(NA_real_, 7),
-    ratio_mother  = rep(NA_real_, 7),
-    ratio_proband = rep(NA_real_, 7),
+    ID = c("S1", "S1", "S1", "S1", "S1", "S1", "S2", "S2"),
+    chromosome = c("1", "1", "1", "1", "1","1","2", "2"),
+    start = c(50, 75, 100, 110, 150, 300, 500, 550),
+    end = c(70, 85, 120, 130, 180, 320, 520, 580),
+    n_snps = c(5, 3, 8, 10, 10, 6, 12, 7),
+    group = c("iso_mat", "iso_mat", "iso_mat", "iso_mat", "iso_mat", "het_pat", "iso_mat", "iso_mat"),
+    n_mendelian_error = c(1, 5, 5, 10, 10,  3, 50, 30),
+    ratio_father  = rep(NA_real_, 8),
+    ratio_mother  = rep(NA_real_, 8),
+    ratio_proband = rep(NA_real_, 8),
     stringsAsFactors = FALSE
   )
 
@@ -21,15 +21,15 @@ expected_result <- data.frame(
     start = c(300,100, 500),
     end = c(320, 180, 580),
     group = c("het_pat","iso_mat", "iso_mat"),
-    n_events = c(1, 2, 2),
-    total_mendelian_error = c(3, 15, 80),
-    total_size = c(20, 50, 50),
-    total_snps = c(6, 18, 19),
-    prop_covered = c(1, 0.625, 0.625),
+    n_events = c(1, 3, 2),
+    total_mendelian_error = c(3, 25, 80),
+    total_size = c(20, 80, 80),
+    total_snps = c(6, 28, 19),
+    prop_covered = c(1, 0.75, 0.625),
     ratio_father  = rep(NA_real_, 3),
     ratio_mother  = rep(NA_real_, 3),
     ratio_proband = rep(NA_real_, 3),
-    collapsed_events = c( "1:300-320","1:100-120,1:150-180", "2:500-520,2:550-580"),
+    collapsed_events = c( "1:300-320","1:100-120,1:110-130,1:150-180", "2:500-520,2:550-580"),
     stringsAsFactors = FALSE
   )
   
@@ -86,7 +86,7 @@ expected_result <- data.frame(
   group = c("het_pat","iso_mat", "iso_mat"),
   n_events = c(1, 2, 2),
   total_mendelian_error = c(3, 15, 80),
-  total_size = c(20, 50, 50),
+  total_size = c(20, 80, 80),
   total_snps = c(6, 18, 19),
   prop_covered = c(1, 0.625, 0.625),
   ratio_father  = c(0.99, 0.97, 1.00),
